28eb9e02b900b703a5bff262fbdd8dc1b3f3b6e3
Sander Johansen <sander.olin@gmail.com>
2023-09-25 23:34:50 +0200

    Finished task 1

--- assignment-9/task-1/main.cpp
+++ assignment-9/task-1/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+ostream &operator<<(ostream &os, const vector<int> &vec) {
+    for (auto &elem : vec) {
+        os << elem << ". ";
+    }
+    return os;
+}
+
+bool isGreaterThan15(int n) {
+    return n > 15;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<int> v1 = {3,3,12,14,17,25,30};
+    cout << "v1: " << v1 << endl;
+
+    auto it = find_if(v1.begin(), v1.end(), isGreaterThan15);
+    cout << "First element in v1 that is greater than 15: " << *it << endl;
+
     return 0;
 }
